d2f77ccd9ce5309a48d3a5f713b9d8e76caf0585
BastiA98 <78684126+BastiA98@users.noreply.github.com>
2021-03-19 13:34:21 +0100

    New

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ let paths = {
 };
 
 let person = {
-  MaxMueller:
+  LuzieRietschel:
     "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-};
+  };
 
 let previousVideos = [];
 let counter = 0;
